4c4a05b153a5cb6ca5432c771053109605c84698
FerrandezBenjamin <58741067+FerrandezBenjamin@users.noreply.github.com>
2020-04-30 15:31:53 +0200

    Supression de titre + mise en place de canvas

---
+++ Algo/server/app.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var http = require("http");
+var app = express();
+
+http.Server(app).listen(80, function () {
+  console.log("listen on 80");
+});
+
+app.use("/", express.static(__dirname + "/../client"));
