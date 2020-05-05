14a642bfb1698b3ec6c29910cc10df9f79012bbd
FerrandezBenjamin <58741067+FerrandezBenjamin@users.noreply.github.com>
2020-05-05 17:05:53 +0200

    Ajout des fonctions pour bouger notre bloc, et commencement des gestions de collisions

--- Algo/client/js/controller.js
+++ Algo/client/js/controller.js
@@ -1,5 +1,18 @@
 function initializeController(grid) {
   window.onkeydown = (event) => {
-    console.log(event.code);
+    switch (event.code) {
+      case "ArrowUp": // Changement d'orientation
+        incOrientation(grid);
+        break;
+      case "ArrowDown": // Descendre pour rapidement mon bloc
+        update(grid);
+        break;
+      case "ArrowLeft": // Allez à gauche
+        decX(grid);
+        break;
+      case "ArrowRight": // Allez à droite
+        incX(grid);
+        break;
+    }
   };
 }
--- Algo/client/js/model.js
+++ Algo/client/js/model.js
@@ -8,7 +8,7 @@ function initializeModel(grid) {
   grid.y = -1;
   initializeGrid(grid);
   chooseBloc(grid);
-  window.setInterval(update, 100, grid);
+  window.setInterval(update, 250, grid);
 }
 function initializeGrid(grid) {
   for (let i = 0; i < grid.height; i++) {
@@ -31,13 +31,42 @@ function chooseBloc(grid) {
   grid.y = 0;
 }
 
+function incOrientation(grid) {
+  grid.orientation = (grid.orientation + 1) % grid.bloc.cells.length;
+}
+
+function decX(grid) {
+  // Pour pas que notre bloc dépasse sur la gauche
+  if (grid.x > 0 && blocCanGoThere(grid, grid.x - 1, grid.y)) {
+    grid.x--;
+  }
+}
+
+function incX(grid) {
+  // Pour pas que notre bloc dépasse sur la droite
+  if (
+    grid.x < grid.width - grid.bloc.cells[grid.orientation][0].length &&
+    blocCanGoThere(grid, grid.x + 1, grid.y)
+  ) {
+    grid.x++;
+  }
+}
+
 function update(grid) {
-  if (grid.y < grid.height - grid.bloc.cells[grid.orientation].length) {
+  // Quand le bloc arrive en bas de la page, stock le bloc, et en créer un autre
+  if (
+    grid.y < grid.height - grid.bloc.cells[grid.orientation].length &&
+    blocCanGoThere(grid, grid.x, grid.y + 1)
+  ) {
     grid.y++;
   } else {
-    stockBloc(grid);
-    chooseBloc(grid);
+    stockBloc(grid); // Stock dans la grille
+    chooseBloc(grid); // Creer un autre bloc
   }
+}
+
+function blocCanGoThere(grid, x, y) {
+  return true;
 }
 
 function stockBloc(grid) {
